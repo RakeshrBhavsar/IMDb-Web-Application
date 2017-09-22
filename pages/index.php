10f5095df26aedcac6f46f6a1b32e942e31e49a4
RakeshrBhavsar <rakesh.r.bhavsar@gmail.com>
2017-09-21 21:16:22 -0400

    First commit

---
+++ pages/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+   <title>AIS Project 1</title>
+   <head>
+      <link type="text/css" rel="stylesheet" href="../resources/page.css">
+	  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	  
+		  <script  src="https://code.jquery.com/jquery-2.2.4.min.js"
+			  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous">
+		</script>
+		
+		<script>
+			$(function(){
+				$('#left-nav').load('left-nav-pane.html');
+			});
+		</script>
+	
+   </head>
+   <body>
+      <div id="container">
+		 
+		 <div>	 
+			<iframe id="header" src="./header.html" name="header" allowTransparency="true" scrolling="no" frameborder="0">
+			</iframe>
+		</div>
+		
+         <div class="gap" >
+         </div>
+         <div class="gap">
+         </div>
+		 
+		 <div id="left-nav"></div>
+		 
+         <div style="height:400px;width:.4%;float:left;">
+         </div>
+		 
+         <div id="content">
+            <div id="container" >
+               <h3 align="center">AIS Project 1 : Client and Server-side Web IS Development  </h3>
+			   <div class="gap"></div>
+			   <h2> Project Description</h2>
+			   
+			   <h4> The Six Degrees of Kevin Bacon is a game based upon the theory that every actor</h4> 
+			   <h4> can be connected to actor Kevin Bacon by a chain of movies no more than 6 in</h4>
+			   <h4> length. Most, but not all, can reach him in 6 steps. 12% of all actors cannot </h4>
+			   <h4> reach him at all.</h4>
+			   
+			  
+			   <p align="center"></p>
+			 
+			   
+			   
+            </div>
+         </div>
+		 
+		 <div>	 
+			<iframe id="footer" src="./footer.html" name="footer" allowTransparency="true" scrolling="no" frameborder="0">
+			</iframe>
+		</div>
+	
+    </div>
+	
+   </body>
+</html> 
